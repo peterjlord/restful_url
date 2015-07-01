f22f06ff3efbd5444ef140f18982f3192098f6d1
Peter J Lord <me@peterjlord.co.uk>
2015-07-01 15:40:04 +0100

    added suport for simple list field

---
+++ plugins/restful/node/urls/1.1/RestfulExampleUrlsResource__1_1.class.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file
+ * Contains RestfulExampleArticlesResource__1_1.
+ */
+
+class RestfulExampleUrlsResource__1_1 extends RestfulExampleUrlsResource {
+  /**
+   * Overrides RestfulExampleArticlesResource::publicFieldsInfo().
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+    unset($public_fields['self']);
+
+    $public_fields['options'] = array(
+      'property' => 'field_options',
+    );
+
+    $public_fields['random-int'] = array(
+      'callback' => 'static::randomNumber',
+    );
+    return $public_fields;
+  }
+  public static function randomNumber($wrapper) {
+    return mt_rand();
+  }
+}
+
